36930666061b4cf034753d51f1dd506fd5894c78
Nami <havenothorn@gmail.com>
2023-11-27 22:00:39 +0900

    feat: Minutes, hour 변환 - State Practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,36 @@
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
-  // counter : 초기값(0), setCounter : 값을 바꾸는 함수(modifier)
-  const handleClick = () => {
-    console.log("Counter Clicked");
-    setCounter((prevCounter) => prevCounter + 1);
+  const [minutes, setMinutes] = useState<number>(0);
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setMinutes(Number(event.target.value));
   };
+  const hours = Math.round(minutes / 60);
+  const reset = () => setMinutes(0);
 
   return (
     <div>
-      <h3>Total Clicks: {counter}</h3>
-      <button onClick={handleClick}>Click me!</button>
+      <h1>Super Converter</h1>
+      <div>
+        <label htmlFor="minutes">Minutes </label>
+        <input
+          value={minutes === 0 ? "" : minutes}
+          id="minutes"
+          placeholder="minutes"
+          type="number"
+          onChange={onChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="hours">Hours </label>
+        <input
+          value={hours === 0 ? "" : hours}
+          id="hours"
+          placeholder="Hours"
+          type="number"
+        />
+      </div>
+      <button onClick={reset}>RESET</button>
     </div>
   );
 };
