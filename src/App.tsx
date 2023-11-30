db8305c722f4acb682e950896c8d0630c3187b0f
Nami <havenothorn@gmail.com>
2023-11-30 22:19:04 +0900

    feat: useEffect Hook 예시 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, ChangeEvent } from "react";
 
 const App = () => {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("I run all the time");
-  const Once = () => {
-    console.log("once");
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setKeyword(event.target.value);
   };
-  useEffect(Once, []);
+  useEffect(() => {
+    console.log("once");
+  }, []);
+  useEffect(() => {
+    console.log("I run when 'keyword' changes");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes");
+  }, [counter]);
   return (
     <>
       <div>
+        <input
+          value={keyword}
+          onChange={onChange}
+          type="text"
+          placeholder="Search here"
+        ></input>
         <h1>{counter}</h1>
         <button onClick={onClick}>Click</button>
       </div>
